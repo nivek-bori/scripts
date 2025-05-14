0bb7ba830c41cf559b20771e02891e9cae3d34cf
nivek-bori <kevinboriboonsomsin@gmail.com>
2025-05-13 19:46:13 -0700

    unconfig spotify file

---
+++ run_unconfig_spotify.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export PS4='+ $(date "+%H:%M:%S") [${BASH_SOURCE##*/}:${LINENO}] '
+printf "\n"
+
+# Constants
+SPOT_LOC="/Applications/Spotify.app"
+TMP_LOC="/tmp/temp_spotify_executable.sh"
+DWNLD_LOC="https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/main/spotx.sh"
+
+# Executed code
+printf "DELETING OLD SPOTIFY\n"
+set -x
+sudo rm -r "$SPOT_LOC"
+set +x
+printf "\n"
+
+printf "CREATING THE TEMPORARY EXECUTION FILE\n"
+set -x
+touch "$TMP_LOC"
+set +x
+printf "\n"
+
+printf "DOWNLOADING CONTENT\n"
+set -x
+curl -ssL "$DWNLD_LOC" -o "$TMP_LOC" # Downloading content
+chmod +x "$TMP_LOC" # Giving permissions
+set +x
+printf "\n"
+
+# DEBUGGING
+# cat "$TMP_LOC"
+
+printf "RUNNING CONTENT\n"
+set -x
+bash "$TMP_LOC"
+set +x
+printf "\n"
+
+rm "$TMP_LOC"
