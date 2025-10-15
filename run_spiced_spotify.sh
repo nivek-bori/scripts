381f9a315003828ff506fa444c68a569f3ece737
nivek-bori <134992572+nivek-bori@users.noreply.github.com>
2025-10-15 06:48:42 -0700

    spicetify auto install extensions

--- run_spiced_spotify.sh
+++ run_spiced_spotify.sh
@@ -7,8 +7,19 @@ printf "\n"
 
 # Constants
 SPOT_LOC="/Applications/Spotify.app"
-TMP_LOC="/tmp/temp_spotify_executable.sh"
+EXEC_LOC="/tmp/temp_spotify_executable.sh"
 DWNLD_LOC="https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/main/spotx.sh"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    SPICETIFY_LOC="$HOME/.spicetify"
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    # Windows (Git Bash or Cygwin)
+    SPICETIFY_LOC="$USERPROFILE/.spicetify"
+else
+    # Linux
+    SPICETIFY_LOC="$HOME/.spicetify"
+fi
 
 # Executed code
 printf "DELETING OLD SPOTIFY\n"
@@ -19,13 +30,13 @@ printf "\n"
 
 printf "CREATING THE TEMPORARY EXECUTION FILE\n"
 set -x
-touch "$TMP_LOC"
+touch "$EXEC_LOC"
 set +x
 printf "\n"
 
 printf "DOWNLOADING CONTENT\n"
 set -x
-curl -ssL "$DWNLD_LOC" -o "$TMP_LOC" # Downloading content
+curl -ssL "$DWNLD_LOC" -o "$EXEC_LOC" # Downloading content
 chmod +x "$TMP_LOC" # Giving permissions
 set +x
 printf "\n"
@@ -35,9 +46,17 @@ printf "\n"
 
 printf "RUNNING CONTENT\n"
 set -x
-printf "y\nn\ny\ny\ny\nn\n" | bash "$TMP_LOC"
+printf "y\nn\ny\ny\ny\nn\n" | bash "$EXEC_LOC"
 set +x
 printf "\n"
+
+printf "DOWNLOADING SPICETIFY EXTENSIONS"
+set -x
+cd SPICETIFY_LOC
+cd Extensions
+curl -o shuffle_plus.js https://raw.githubusercontent.com/spicetify/cli/refs/heads/main/Extensions/shuffle%2B.js
+curl -o scannables.js https://raw.githubusercontent.com/ohitstom/spicetify-extensions/refs/heads/main/scannables/scannables.js
+curl -o beautiful_lyrics.mjs https://raw.githubusercontent.com/surfbryce/beautiful-lyrics/refs/heads/main/Builds/Release/beautiful-lyrics.mjs
 
 printf "APPLYING SPICETIFY\n"
 set -x
@@ -45,6 +64,9 @@ chmod +x /opt/homebrew/bin/spicetify
 spicetify backup apply
 spicetify config current_theme Text
 spicetify config color_scheme Catppuccin
+spicetify config extensions shuffle_plus.js
+spicetify config extensions scannables.js
+spicetify config extensions beautiful lyrics.mjs
 spicetify apply
 set +x
 printf "\n"
